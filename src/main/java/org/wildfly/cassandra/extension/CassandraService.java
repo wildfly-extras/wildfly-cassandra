e85ddf2ce75c05d93e8cd018c43222b3d92f0e8d
Heiko Braun <ike.braun@googlemail.com>
2014-08-08 14:11:54 +0200

    Packaging and starting daemon (fails due to access of sun.misc.Unsafe)

--- src/main/java/org/wildfly/cassandra/extension/CassandraService.java
+++ src/main/java/org/wildfly/cassandra/extension/CassandraService.java
@@ -1,11 +1,18 @@
 package org.wildfly.cassandra.extension;
 
+import org.apache.cassandra.exceptions.ConfigurationException;
+import org.apache.cassandra.service.CassandraDaemon;
+import org.apache.thrift.transport.TTransportException;
 import org.jboss.msc.service.Service;
 import org.jboss.msc.service.ServiceName;
 import org.jboss.msc.service.StartContext;
 import org.jboss.msc.service.StartException;
 import org.jboss.msc.service.StopContext;
 import org.wildfly.cassandra.logging.CassandraLogger;
+import org.wildfly.cassandra.server.EmbeddedServerHelper;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author Heiko Braun
@@ -14,6 +21,7 @@ public class CassandraService implements Service<CassandraService> {
 
 
     private final String suffix;
+    private CassandraDaemon cassandraDaemon;
 
     public CassandraService(String suffix) {
         this.suffix = suffix;
@@ -27,11 +35,24 @@ public class CassandraService implements Service<CassandraService> {
     @Override
     public void start(StartContext context) throws StartException {
         CassandraLogger.LOGGER.infof("Starting embedded cassandra service '%s'.", suffix);
+        try {
+            cassandraDaemon = new EmbeddedServerHelper().startEmbeddedCassandra(
+                    new File(System.getProperty("user.home") + "/cassandra.yaml"),
+                    System.getProperty("java.io.tmpdir"),
+                    3000
+            );
+        } catch (Throwable e) {
+            context.failed(new StartException(e));
+        }
     }
 
     @Override
     public void stop(StopContext context) {
         CassandraLogger.LOGGER.infof("Stopping cassandra service '%s'.", suffix);
+        if(cassandraDaemon!=null)
+        {
+            cassandraDaemon.deactivate();
+        }
     }
 
     public static ServiceName createServiceName(String suffix) {
