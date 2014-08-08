f0c69bfd3c2186cc72223faa9094e393855536b8
Heiko Braun <ike.braun@googlemail.com>
2014-08-08 12:30:15 +0200

    revert to system out logging until the loggers actually work

--- src/main/java/org/wildfly/cassandra/extension/CassandraService.java
+++ src/main/java/org/wildfly/cassandra/extension/CassandraService.java
@@ -5,6 +5,7 @@ import org.jboss.msc.service.ServiceName;
 import org.jboss.msc.service.StartContext;
 import org.jboss.msc.service.StartException;
 import org.jboss.msc.service.StopContext;
+import org.wildfly.cassandra.logging.CassandraLogger;
 
 /**
  * @author Heiko Braun
@@ -25,12 +26,14 @@ public class CassandraService implements Service<CassandraService> {
 
     @Override
     public void start(StartContext context) throws StartException {
-        System.out.println("Starting cassandra service");
+        //CassandraLogger.LOGGER.infof("Starting embedded cassandra service '%s'.", suffix);
+        System.out.println("Starting embedded cassandra service");
     }
 
     @Override
     public void stop(StopContext context) {
-        System.out.println("Stopping cassandra service");
+        //CassandraLogger.LOGGER.infof("Stopping cassandra service '%s'.", suffix);
+        System.out.println("Stopping embedded cassandra service");
     }
 
     public static ServiceName createServiceName(String suffix) {
